231970e3bf70b78767657ad9e76863f472717d40
clemens031 <clemensheckmann@web.de>
2025-07-23 09:45:06 +0200

    Update coreview.py

--- arguments/zju_mocap/coreview.py
+++ arguments/zju_mocap/coreview.py
@@ -5,16 +5,16 @@ ModelHiddenParams = dict(
      'grid_dimensions': 2,
      'input_coordinate_dim': 4,
      'output_coordinate_dim': 32,
-     'resolution': [64, 64, 64, 75]
+     'resolution': [64, 64, 64, 16]  # just 15 frames + buffer
     }
 )
 
 
 OptimizationParams = dict(
     use_motion_pruning = True,
-    low_motion_threshold = 0.08,
+    low_motion_threshold = 0.05,  # prune more agressively because of noisy images 
     use_low_transform_pruning = True,
-    low_transform_threshold = 0.08,
+    low_transform_threshold = 0.05,
     min_point_number = 10000,
     supplement_point_number = 5000,
     pruning_from_iter = 1000,
